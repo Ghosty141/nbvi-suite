0bc30e90a7cd71f450ca173b333d63ff55d7741d
Ernie Rael <err@raelity.com>
2007-09-02 00:48:23 +0000

    - Refactoring: move file related stuff out of textview into buffer and fs.

--- src/org/netbeans/modules/jvi/NbFS.java
+++ src/org/netbeans/modules/jvi/NbFS.java
@@ -1,6 +1,7 @@
 package org.netbeans.modules.jvi;
 
 import com.raelity.jvi.Msg;
+import com.raelity.jvi.ViBuffer;
 import com.raelity.jvi.ViFS;
 import com.raelity.jvi.ViManager;
 import com.raelity.jvi.ViTextView;
@@ -17,8 +18,8 @@ import org.openide.windows.TopComponent;
 
 public class NbFS implements ViFS
 {
-    private DataObject getDataObject(ViTextView tv) {
-	Document doc = tv.getEditorComponent().getDocument();
+    private DataObject getDataObject(ViBuffer buf) {
+	Document doc = (Document)buf.getDocument();
         DataObject dobj = null;
 	if(doc != null) {
 	    FileObject fo = NbEditorUtilities.getFileObject(doc);
@@ -31,24 +32,47 @@ public class NbFS implements ViFS
         return dobj;
     }
 
-    public boolean isModified(ViTextView tv) {
-        DataObject dobj = getDataObject(tv);
+    public String getDisplayFileName(ViBuffer buf) {
+        Document doc = (Document)buf.getDocument();
+        if(doc != null) {
+            FileObject fo = NbEditorUtilities.getFileObject(doc);
+            /*System.err.println("getPath = " + fo.getPath());
+            System.err.println("getFileDisplayName = "
+                               + FileUtil.getFileDisplayName(fo));
+            System.err.println("toFileAbs = " + FileUtil.toFile(fo).getAbsolutePath());
+            System.err.println("toFileRel = " + FileUtil.toFile(fo).getPath());
+            System.err.println("toFileParent = " + FileUtil.toFile(fo).getParent());*/
+            if(fo != null)
+                return fo.getNameExt();
+        }
+        return "UNKNOWN";
+    }
+
+    public boolean isModified(ViBuffer buf) {
+        DataObject dobj = getDataObject(buf);
         return dobj != null ? dobj.isModified() : true;
     }
 
+    public boolean isReadOnly(ViBuffer buf) {
+        //DataObject dobj = getDataObject(buf);
+        //return dobj != null ? dobj.isReadOnly() : true;
+        return false;
+    }
+
     public void write(ViTextView tv, boolean force) {
-        DataObject dobj = getDataObject(tv);
+        ViBuffer buf = tv.getBuffer();
+        DataObject dobj = getDataObject(buf);
         if(dobj != null) {
             //SaveAction s = (SaveAction)SystemAction.get(SaveAction.class);
             // NB6.0 s.createContextAwareInstance(dobj.Lookup());
             //s.performAction((Node[])null);
-            SaveCookie sc = (SaveCookie)dobj.getCookie(SaveCookie.class);
+            SaveCookie sc = dobj.getCookie(SaveCookie.class);
             if(sc != null) {
                 try {
                     sc.save();
-                    Msg.smsg(tv.getDisplayFileNameAndSize() + " written");
+                    Msg.smsg(buf.getDisplayFileNameAndSize() + " written");
                 } catch (IOException ex) {
-                    Msg.emsg("error writing " + tv.getDisplayFileName());
+                    Msg.emsg("error writing " + buf.getDisplayFileName());
                 }
             } else {
                 // Msg.wmsg(fo.getNameExt() + " not dirty");
@@ -57,7 +81,7 @@ public class NbFS implements ViFS
     }
 
     public void writeAll(boolean force) {
-	SaveAllAction sa = (SaveAllAction)SystemAction.get(SaveAllAction.class);
+	SaveAllAction sa = SystemAction.get(SaveAllAction.class);
         sa.performAction();
     }
 
@@ -66,13 +90,13 @@ public class NbFS implements ViFS
 	Msg.emsg("WRITE new_name NOT IMPLEMENTED, " + force);
     }
 
-    public void edit(ViTextView viTextView, int i, boolean force) {
+    public void edit(ViTextView tv, int i, boolean force) {
 	TopComponent tc = (TopComponent)ViManager.getTextBuffer(i);
 	if(tc == null) {
 	  Msg.emsg("No alternate file name to substitute for '#" + i + "'");
 	  return;
 	}
 	tc.requestActive();
-        Msg.smsg(viTextView.getDisplayFileNameAndSize());
+        Msg.smsg(tv.getBuffer().getDisplayFileNameAndSize());
     }    
 }

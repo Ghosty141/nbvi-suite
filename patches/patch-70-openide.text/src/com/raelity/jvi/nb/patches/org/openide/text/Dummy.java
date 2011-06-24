c59ee4f36539a59e14919aa1693d6e96ffc25ca1
Ernie Rael <err@raelity.com>
2011-06-24 15:50:32 -0700

    the files Patch 7.0 UndoGroupManager to ignore empty begin/end group. bug 150251

---
+++ patches/patch-70-openide.text/src/com/raelity/jvi/nb/patches/org/openide/text/Dummy.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.raelity.jvi.nb.patches.org.openide.text;
+
+/**
+ *
+ * @author Ernie Rael <err at raelity.com>
+ */
+public class Dummy {
+
+}

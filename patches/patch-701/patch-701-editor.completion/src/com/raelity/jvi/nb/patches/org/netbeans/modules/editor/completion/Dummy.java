1beb0137ef8cc570373be6a47df0e093875dd2cf
Ernie Rael <err@raelity.com>
2011-08-04 09:32:33 -0700

    Add editor completion patch. Some cleanup.

---
+++ patches/patch-701/patch-701-editor.completion/src/com/raelity/jvi/nb/patches/org/netbeans/modules/editor/completion/Dummy.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.raelity.jvi.nb.patches.org.netbeans.modules.editor.completion;
+
+/**
+ *
+ * @author Ernie Rael <err at raelity.com>
+ */
+public class Dummy {
+
+}
